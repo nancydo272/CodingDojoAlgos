0dc91544bf2ba9916b287b970c881047eed25087
Nancy Do <nancydo272@gmail.com>
2022-11-25 12:52:10 -0500

    2nd algo

---
+++ PreReq/printOddNumbers.js
@@ -0,0 +1,12 @@
+//Write a program that would print all the odd numbers from 1 to 1000
+function printOddNumbers(){
+    for(var i = 1; i <1001; i+=2){
+        console.log(i);
+    }
+    return i; 
+}
+
+
+printOddNumbers(); 
+
+//remember that the + is in front of the = --> +=
