be9f45e1a6d80e0daa3ceb6a28bfe971270ff8f1
Nancy Do <nancydo272@gmail.com>
2022-11-29 09:47:49 -0500

    10th algo

---
+++ PreReq/eliminateNegativeNum.js
@@ -0,0 +1,18 @@
+//Given an array x (e.g. [1,5, 10, -2]), create an algorithm (sets of instructions) that 
+//replaces any negative number with the value of 0.  When the program is done x should have
+// no negative values (e.g. [1, 5, 10, 0]).
+
+var x = [1, 5, 10, -2]; 
+
+
+function eliminateNeg(){
+    for(var i = 0; i < x.length; i++){
+        if(x[i]< 0 ){
+            x[i]=0; 
+        }
+    }
+    console.log(x); 
+    return x;
+}
+
+eliminateNeg(); 
