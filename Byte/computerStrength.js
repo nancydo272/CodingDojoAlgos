8ac393050f2d01a43c97314c6944c314389b90e8
Nancy Do <nancydo272@gmail.com>
2022-11-30 10:50:24 -0500

    just something to upload for later

---
+++ Byte/computerStrength.js
@@ -0,0 +1,9 @@
+//A computer is really good at two things. It has a lot of bits which means we can store a lot 
+//of different representations. It is also really good at doing some task over and over again. 
+//Using this powerful looping power of computers is also know as iteration. 
+//You iterate over some block of code over and over again until a condition is met.
+
+var arr = [24, 8, 23, 3];
+linearSearch(8, arr);   // => 1
+linearSearch(-99, arr); // => false
+
