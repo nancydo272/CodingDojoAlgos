6f715ca87222d6a2df7632df8f85a99056b78728
Nancy Do <nancydo272@gmail.com>
2022-12-02 20:54:36 -0500

    struggle

--- projectAlgoCodingDojo/arrays/arrayToDo1.js
+++ projectAlgoCodingDojo/arrays/arrayToDo1.js
@@ -20,22 +20,42 @@
 //when you want to shift to the right, you start at the end and subtract. 
 //when you want to shift to the let, you start at the begining and add.
 
-//2. Given an array, remove and return the value at the beginning of the array. 
+//2. popFront
+//Given an array, remove and return the value at the beginning of the array. 
 //Prove the value is removed from the array by printing it. 
 //You may use .pop(), you are able do this without it though!
     //Examples:
     //popFront([0,5,10,15]) => 0 returned, with [5,10,15] printed in the function
     //popFront([4,5,7,9]) => 4 returned, with [5,7,9] printed in the function
 
-    function popFront(array){
-        var temp = array[0];
-        for(var i = 0; i < array.length; i++){
-            array[i]=array[i+1];
+    // function popFront(array){
+    //     var temp = array[0];
+    //     for(var i = 0; i < array.length; i++){
+    //         array[i]=array[i+1];
+    //     }
+    //     array.length = array.length -1;
+    //     //need this line, will not work w/o it cause its a variable now;
+    //     console.log(array);
+    //     return temp; 
+    // }
+    // console.log(popFront([0,5,10,15]));
+    // console.log(popFront([4,5,7,9]));
+
+//3. Insert At
+//Given an array, index, and additional value, insert the value into array at given index. 
+//You can think of pushFront(arr,val) as equivalent to insertAt(arr,0,val). 
+//You may use .push(), you are able do this without it though!
+    //Examples:
+
+    //insertAt([100,200,5], 2, 311) => [100,200,311,5]
+    //insertAt([9,33,7], 1, 42) => [9,42,33,7]
+
+    function insertAt(arr, j, val){
+        for(var i = arr.length; i>j; i--){
+            arr[i]=arr[i-1];
         }
-        array.length = array.length -1;
-        //need this line, will not work w/o it cause its a variable now;
-        console.log(array);
-        return temp; 
+        arr[j]= val;
+        console.log(arr)
     }
-    console.log(popFront([0,5,10,15]));
-    console.log(popFront([4,5,7,9]));
+    console.log(insertAt([100,200,5], 2, 311));
+    console.log(insertAt([9,33,7], 1, 42));
