2606243af756f9b8d75f6a41f5fb9d99acd1b338
Nancy Do <nancydo272@gmail.com>
2022-12-04 10:20:21 -0500

    strength through struggle

--- projectAlgoCodingDojo/arrays/arrayToDo1.js
+++ projectAlgoCodingDojo/arrays/arrayToDo1.js
@@ -6,16 +6,16 @@
     //pushFront([5,7,2,3], 8) => [8,5,7,2,3]
     //pushFront([99], 7) => [7,99]
 
-//     function pushFront(x, val){
-//         for(var i=x.length; i>0; i--){
-//             x[i]=x[i-1]
-//         }
-//         x[0]= val; 
-//         console.log(x);
-//         return x; 
-//     }
-// pushFront([5,7,2,3], 8);
-// pushFront([99], 7);
+    function pushFront(x, val){
+        for(var i=x.length; i>0; i--){
+            x[i]=x[i-1]
+        }
+        x[0]= val; 
+        console.log(x);
+        return x; 
+    }
+pushFront([5,7,2,3], 8);
+pushFront([99], 7);
 
 //when you want to shift to the right, you start at the end and subtract. 
 //when you want to shift to the let, you start at the begining and add.
@@ -28,18 +28,18 @@
     //popFront([0,5,10,15]) => 0 returned, with [5,10,15] printed in the function
     //popFront([4,5,7,9]) => 4 returned, with [5,7,9] printed in the function
 
-    // function popFront(array){
-    //     var temp = array[0];
-    //     for(var i = 0; i < array.length; i++){
-    //         array[i]=array[i+1];
-    //     }
-    //     array.length = array.length -1;
-    //     //need this line, will not work w/o it cause its a variable now;
-    //     console.log(array);
-    //     return temp; 
-    // }
-    // console.log(popFront([0,5,10,15]));
-    // console.log(popFront([4,5,7,9]));
+    function popFront(array){
+        var temp = array[0];
+        for(var i = 0; i < array.length; i++){
+            array[i]=array[i+1];
+        }
+        array.length = array.length -1;
+        //need this line, will not work w/o it cause its a variable now;
+        console.log(array);
+        return temp; 
+    }
+    console.log(popFront([0,5,10,15]));
+    console.log(popFront([4,5,7,9]));
 
 //3. Insert At
 //Given an array, index, and additional value, insert the value into array at given index. 
@@ -50,15 +50,15 @@
     //insertAt([100,200,5], 2, 311) => [100,200,311,5]
     //insertAt([9,33,7], 1, 42) => [9,42,33,7]
 
-    // function insertAt(arr, j, val){
-    //     for(var i = arr.length; i>j; i--){
-    //         arr[i]=arr[i-1];
-    //     }
-    //     arr[j]= val;
-    //     console.log(arr)
-    // }
-    // console.log(insertAt([100,200,5], 2, 311));
-    // console.log(insertAt([9,33,7], 1, 42));
+    function insertAt(arr, j, val){
+        for(var i = arr.length; i>j; i--){
+            arr[i]=arr[i-1];
+        }
+        arr[j]= val;
+        console.log(arr)
+    }
+    console.log(insertAt([100,200,5], 2, 311));
+    console.log(insertAt([9,33,7], 1, 42));
 
 //4. Remove At 
 //Given an array and an index into array, remove and return the array value at that index. 
@@ -68,18 +68,18 @@
     //removeAt([1000,3,204,77], 1) => 3 returned, with [1000,204,77] printed in the function
     //removeAt([8,20,55,44,98], 3) => 44 returned, with [8,20,55,98] printed in the function
 
-    // function removeAt(arr, j){
-    //     var temp = arr[j];
-    //     for(var i = j; i<arr.length; i++){
-    //         arr[i]=arr[i+1];
-    //     }
-    //     arr.length = arr.length-1;
-    //     console.log(arr);
-    //     // console.log(temp);
-    //     return temp; 
-    // }
-    // console.log(removeAt([1000,3,204,77], 1));
-    // console.log(removeAt([8,20,55,44,98], 3));
+    function removeAt(arr, j){
+        var temp = arr[j];
+        for(var i = j; i<arr.length; i++){
+            arr[i]=arr[i+1];
+        }
+        arr.length = arr.length-1;
+        console.log(arr);
+        // console.log(temp);
+        return temp; 
+    }
+    console.log(removeAt([1000,3,204,77], 1));
+    console.log(removeAt([8,20,55,44,98], 3));
 
 //5. Swap Pairs 
 //Swap positions of successive pairs of values of given array. 
@@ -99,3 +99,4 @@ function insertAt(arr){
 }
 console.log(insertAt([1,2,3,4]));
 console.log(insertAt(["Brendan",true,42]));
+
