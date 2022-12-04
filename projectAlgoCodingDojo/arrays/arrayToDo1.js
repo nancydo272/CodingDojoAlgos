d13555cd74fda46a4beb2f69a9ee3e3afeabc625
Nancy Do <nancydo272@gmail.com>
2022-12-03 21:47:09 -0500

    bonus problems

--- projectAlgoCodingDojo/arrays/arrayToDo1.js
+++ projectAlgoCodingDojo/arrays/arrayToDo1.js
@@ -59,3 +59,11 @@
     }
     console.log(insertAt([100,200,5], 2, 311));
     console.log(insertAt([9,33,7], 1, 42));
+
+//4. Remove At 
+//Given an array and an index into array, remove and return the array value at that index. 
+//Prove the value is removed from the array by printing it. 
+//Think of popFront(arr) as equivalent to removeAt(arr,0).
+    //Examples:
+    //removeAt([1000,3,204,77], 1) => 3 returned, with [1000,204,77] printed in the function
+    //removeAt([8,20,55,44,98], 3) => 44 returned, with [8,20,55,98] printed in the function
