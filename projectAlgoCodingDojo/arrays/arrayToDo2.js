f791d245419c566a816f9ad171b925f419150db3
Nancy Do <nancydo272@gmail.com>
2022-12-05 19:54:16 -0500

    reverse the same array

--- projectAlgoCodingDojo/arrays/arrayToDo2.js
+++ projectAlgoCodingDojo/arrays/arrayToDo2.js
@@ -7,7 +7,34 @@
 //As always, do not use built-in array functions such as splice().
 
 function reverseArray(array){
-
+    for(var i = 0; i<array.length/2; i++){
+        var temp = array[i]; 
+        array[i]= array[array.length-1-i]; 
+        array[array.length-1-i] = temp;
+    }
     console.log(array); 
 }
 reverseArray([1, 2, 3, 4, 5]); 
+//use array.length/2 b/c if not it would give the same array cause it would unreverse what we just reversed. 
+
+//2. Rotate
+//Implement rotateArr(arr, shiftBy) that accepts array and offset. 
+//Shift arr's values to the right by that amount. 'Wrap-around' any values 
+//that shift off array's end to the other side, so that no data is lost. 
+//Operate in-place: given ([1,2,3],1), 
+///change the array to [3,1,2]. Don't use built-in functions.
+//Second: allow negative shiftBy (shift L, not R).
+//Third: minimize memory usage. With no new array, handle arrays/shiftBys in the millions.
+//Fourth: minimize the touches of each element.
+
+//3. Filter Range
+//Alan is good at breaking secret codes. One method is to eliminate values that lie 
+//outside of a specific known range. Given arr and values min and max, retain only the 
+//array values between min and max. Work in-place: return the array you are given, 
+//with values in original order. No built-in array functions.
+
+//4.Concat
+//Replicate JavaScript's concat(). Create a standalone function that accepts two arrays. 
+//Return a new array containing the first array's elements, followed by the second array's elements. 
+//Do not alter the original arrays. Ex.: arrConcat( ['a','b'], [1,2] ) should return 
+//new array ['a','b',1,2].
