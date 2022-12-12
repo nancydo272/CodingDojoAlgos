6a2b709f9a0c9dce39a8cbc424ad7771fdbf9c59
Nancy Do <nancydo272@gmail.com>
2022-12-12 14:02:22 -0500

    more on arrays

---
+++ projectAlgoCodingDojo/arrays/arrayToDo3.js
@@ -0,0 +1,34 @@
+//Remove Negatives
+// Implement removeNegatives() that accepts an array, removes negative values, and 
+//returns the same array (not a copy), preserving non-negatives’ order. As always, do not use built-in array functions.
+
+
+
+// Second-to-Last
+// Return the second-to-last element of an array. Given [42,true,4,"Kate",7], 
+//return "Kate". If array is too short, return null.
+
+
+
+// Second-Largest
+// Return the second-largest element of an array. Given [42,1,4,Math.PI,7], return 7. 
+//If the array is too short, return null.
+
+
+
+// Nth-to-Last
+// Return the element that is N-from-array’s-end. Given ([5,2,3,6,4,9,7],3), return 4. 
+//If the array is too short, return null.
+
+
+
+// Nth-Largest
+// Liam has "N" number of Green Belt stickers for excellent Python projects. Given arr and N, 
+//return the Nth-largest element, where (N-1) elements are larger. Return null if needed.
+
+
+
+// Skyline Heights
+// Lovely Burbank has a breathtaking view of the Los Angeles skyline. Let’s say you are given an array with 
+//heights of consecutive buildings, starting closest to you and extending away. Array [-1,7,3] would represent three buildings: first is actually out of view below street level, behind it is second at 7 stories high, third is 3 stories high (hidden behind the 7-story). You are situated at street level. Return array containing heights of buildings you can see, in order. Given [-1,1,1,7,3] return [1,7]. Given [0,4] return [4]. As always with challenges, 
+//do not use built-in array functions such as unshift().
